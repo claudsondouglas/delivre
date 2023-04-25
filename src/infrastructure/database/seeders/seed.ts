ec95711d1cecc8e3521199460c4b926d54b90297
Claudson Douglas <claudsondouglas.13@gmail.com>
2023-04-24 23:57:44 -0300

    🌱 seeds

--- src/infrastructure/database/seeders/seed.ts
+++ src/infrastructure/database/seeders/seed.ts
@@ -1,5 +1,7 @@
+import CreateProduct from "@cases/product/CreateProduct.case";
 import CreateUser from "@cases/user/CreateUser.case";
 import Bcrypt from "@infrastructure/hashing/Bcrypt";
+import ProductRepository from "@repositories/Product.respository";
 import UserRepository from "@repositories/User.repository";
 
 export default class Seed {
@@ -15,6 +17,63 @@ export default class Seed {
             email: "admin@email.com",
             password: "123456",
         });
+
+        const productRepository = new ProductRepository();
+
+        const pizzas = [
+            // Pizza de Calabresa
+            { 
+              name: "Pizza de Calabresa P", 
+              description: "Pizza de calabresa com muçarela e molho de tomate", 
+              price: 25.00 
+            },
+            { 
+              name: "Pizza de Calabresa M", 
+              description: "Pizza de calabresa com muçarela e molho de tomate", 
+              price: 35.00 
+            },
+            { 
+              name: "Pizza de Calabresa G", 
+              description: "Pizza de calabresa com muçarela e molho de tomate", 
+              price: 45.00 
+            },
+            // Pizza de Mussarela
+            { 
+              name: "Pizza de Mussarela P", 
+              description: "Pizza de mussarela com molho de tomate", 
+              price: 20.00 
+            },
+            { 
+              name: "Pizza de Mussarela M", 
+              description: "Pizza de mussarela com molho de tomate", 
+              price: 30.00 
+            },
+            { 
+              name: "Pizza de Mussarela G", 
+              description: "Pizza de mussarela com molho de tomate", 
+              price: 40.00 
+            },
+            // Pizza de Frango
+            { 
+              name: "Pizza de Frango P", 
+              description: "Pizza de frango com muçarela e molho de tomate", 
+              price: 27.00 
+            },
+            { 
+              name: "Pizza de Frango M", 
+              description: "Pizza de frango com muçarela e molho de tomate", 
+              price: 37.00 
+            },
+            { 
+              name: "Pizza de Frango G", 
+              description: "Pizza de frango com muçarela e molho de tomate", 
+              price: 47.00 
+            },
+          ];
+
+        pizzas.map((pizza: any) => {
+            new CreateProduct(productRepository).execute(pizza);
+        })
     }
 }
 
