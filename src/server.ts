6e00651d25ea9a89b16cbcbc7fdcc6fc3cf6dba2
Claudson Douglas <claudsondouglas.13@gmail.com>
2023-05-28 00:23:58 -0300

    🔄 package: cors

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,17 @@
 import autoLoad from '@fastify/autoload'
 import { join } from 'path'
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 
 const app = fastify()
+app.register(cors, { 
+  origin: '*',
+})
 
 app.register(autoLoad, {
   dir: join(__dirname, 'infrastructure', 'routes'),
 })
 
 app.listen({ port: 3001 })
-
-/**
-app.use(express.json());
-app.use(cors())
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
-});
-
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
-app.use('/product', productRouter);
- */
 
 export default app;
