a2e04e1119fc061f559c50ca73b078d94f26ef9a
Claudson Douglas <claudsondouglas.13@gmail.com>
2023-05-31 17:41:54 -0300

    teste

--- src/infrastructure/middlewares/Auth.middleware.ts
+++ src/infrastructure/middlewares/Auth.middleware.ts
@@ -3,18 +3,20 @@ import Jwt from "@infrastructure/cryptography/Jwt"
 /**
  * fastify onrequest hook
  */
-export default function (request: any, reply: any, done: any) {
+export default async function (request: any, reply: any, done: any) {
     if (!request.headers.authorization) {
         reply.code(401).send({ error: 'Unauthorized' })
     }
 
     const token = request.headers.authorization.replace("Bearer ", "")
     const tokenizer = new Jwt;
-    const valid = new Verify(tokenizer).execute(token)
+    const valid = await (new Verify(tokenizer)).execute(token)
 
     if (!valid) {
         reply.code(401).send({ error: 'Unauthorized' })
     }
+
+    request.user = valid;
 
     done()
 }
--- src/infrastructure/routes/product/product.routes.ts
+++ src/infrastructure/routes/product/product.routes.ts
@@ -3,16 +3,34 @@ import AuthMiddleware from '@infrastructure/middlewares/Auth.middleware';
 
 const controller = new ProductController();
 
+const postProductSchema = {
+    type: 'object',
+    required: ['name', 'description', 'price'],
+    properties: {
+        name: { type: 'string' },
+        description: { type: 'string' },
+        price: { type: 'number' }
+    },
+}
+
 export default async function authRouter(fastify: any, opts: any) {
-    fastify.get('/', controller.index);
+    fastify.get('/user/:id', controller.index);
     fastify.get('/:id', controller.show);
     fastify.post('/', {
-        onRequest: AuthMiddleware
+        onRequest: AuthMiddleware,
+        schema: { 
+            removeAdditional: true,
+            body: postProductSchema
+        }
     }, controller.store);
     fastify.patch('/:id', {
-        onRequest: AuthMiddleware
+        onRequest: AuthMiddleware,
+        schema: { 
+            removeAdditional: true,
+            body: postProductSchema
+        }
     }, controller.update);
     fastify.delete('/:id', {
         onRequest: AuthMiddleware
-    }, controller.delete);    
+    }, controller.delete);
 }
